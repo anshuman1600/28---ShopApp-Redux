57a70ca7d61b5283de6fb574ff00a04765095a10
anshuman1600 <thakuranshumansingh945@gmail.com>
2023-09-16 12:48:58 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+ git config --global user.email thakuranshumansingh945@gmail.com
